e2cb3e93768f1f7d88d244ca0cda3b2cca65cd70
Kyle David <103971670+kyle-david1@users.noreply.github.com>
2022-08-28 15:27:29 -0500

    REACT layout

---
+++ src/resumeData.js
@@ -0,0 +1,105 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Kyle Baca",
+    "role": "Software Engineer",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": " Well versed in front end and backend web development. I am more drawn towards back end devlopment as I find it more straightforward and less ambiguous. I enjoy building react apps and using using querying languages like Mongoose, Sequelize, and GraphQL. When not practicing coding concepts, I prioritze swimming, running, weightlifting, and playing with my dog.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/kyle-baca-084072238/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/kyle-david1",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"Recently achieved a certificate for full stack web developement from the University of Texas at Austin. I am excited to bring the range of coding skills I learned in this setting to my first role as a developer. Some of these Languages and technologies include HTML, CSS, Javascript, OOP, Handlebars and React, Express, Node, Sequelize, Mongoose and GraphQL, to name a few. I live by the principle of being a life long student.",
+    "address":"Austin, Texas",
+    "email":"kyle.baca8@gmail.com",
+    "education":[
+      {
+        "UniversityName":"University of Texas at Austin",
+        "specialization":"full stack web development",
+        "MonthOfPassing":"September",
+        "YearOfPassing":"2022",
+        // "Achievements":""
+      },
+      {
+        "UniversityName":"Austin Community College",
+        "specialization":"Nursing",
+        // "MonthOfPassing":"Jan",
+        // "YearOfPassing":"2018",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Recovery Unplugged",
+        "specialization":"Behavioral Health Technician",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2022",
+        "Achievements":"30% lower client recidivism year over year"
+      },
+      {
+        "CompanyName":"Nova Recovery",
+        "specialization":"Behavioral Health Technician",
+        "MonthOfLeaving":"April",
+        "YearOfLeaving":"2021",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Express"
+      },
+      {
+        "skillname":"React.js"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"webapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
